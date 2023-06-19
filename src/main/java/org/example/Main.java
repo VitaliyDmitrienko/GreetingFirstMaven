991c03a5b5b8a06eaea656491d94280e674af200
VitaliyDmitrienko <vddjack2008@yandex.ru>
2023-06-19 21:51:53 +0300

    1st.

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,14 @@
+package org.example;
+
+import lombok.Data;
+import org.junit.Test;
+
+public class Main {
+    public static void main(String[] args) {
+
+//        @Test
+
+        System.out.println("Hello world!");
+        System.out.println("Greeting from First Maven Project!!!");
+    }
+}
